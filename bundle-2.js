4dccaa9e8a6f883a99c5b90ea07b83ec09da4959
davidsauntson <davidsauntson@email.com>
2016-10-27 21:57:36 +0100

    initial commit

---
+++ bundle-2.js
@@ -0,0 +1,12 @@
+var myModule = (function () {
+'use strict';
+
+var foo = 42;
+
+var main = function () {
+  console.log(foo);
+};
+
+return main;
+
+}());
---
+++ bundle.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var foo = 42;
+
+var main = function () {
+  console.log(foo);
+};
+
+module.exports = main;
